61792fcca64d6b57d3de2e34d37f912e1936bd34
Johan Norberg <johan.norberg@jepono.se>
2024-02-27 20:16:11 +0100

    bkp

--- zero_ecs_macros/src/lib.rs
+++ zero_ecs_macros/src/lib.rs
@@ -1,1 +1,11 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
 
+#[proc_macro_attribute]
+pub fn just_for_test(_: TokenStream, input: TokenStream) -> TokenStream {
+    let output = format!(
+        "fn my_test() {{ println!(\"Macro just_for_test invoked\"); {} }}",
+        input.to_string()
+    );
+    output.parse().unwrap()
+}
--- zero_ecs_testbed/src/main.rs
+++ zero_ecs_testbed/src/main.rs
@@ -1,3 +1,11 @@
+use zero_ecs::just_for_test;
+
+#[just_for_test]
+fn my_test() {
+    println!("hej");
+}
+
 fn main() {
+    my_test();
     println!("Hello, world!");
 }
